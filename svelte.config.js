c74df665c15d36fa6e19ed5be02474347ed22279
DragonSlyme <contact@jules-autret.dev>
2025-01-16 11:44:07 +0100

    add projects entries

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { breizhIle } from './src/data/projects/projects.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,14 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({}),
 		prerender: {
-			entries: ["*"]
+			entries: [
+				"/projects/breizh-ile",
+				"/projects/puissance4",
+				"/projects/LLTK",
+				"/projects/outil-info-schema",
+				"/projects/team-building",
+				"/projects/automatisation",
+			]
 		}
 	}
 };
