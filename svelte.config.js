9b1f330495e3df3ade788a213ab27a9102887e3d
DragonSlyme <contact@jules-autret.dev>
2025-01-16 11:46:35 +0100

    removed unused import

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { breizhIle } from './src/data/projects/projects.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
